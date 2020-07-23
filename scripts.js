4df4ac77e9fbda356a63e3d158f266bea6579982
PatrykH-web <68401266+PatrykH-web@users.noreply.github.com>
2020-07-24 00:21:35 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,48 @@
+$(".burger").on("click", function () {
+    $(".burger").toggleClass("change");
+    $(".nav").toggleClass("change");
+})
+
+$('.check-btn').on("click", function () {
+    $(window).scrollTop($contactFromTop);
+})
+
+let $realizationFromTop = $('.realization').offset().top;
+let $locationsFromTop = $('.locations').offset().top;
+let $opinionsFromTop = $('.opinions').offset().top;
+let $contactFromTop = $('.contact-section').offset().top;
+
+let $navHeight = $('.nav').outerHeight();
+let $menuHeight = $('.menu').outerHeight();
+
+$('.two').on('click', function () {
+    if ($(window).innerWidth() > 1024) {
+        $(window).scrollTop($realizationFromTop);
+    } else {
+        $(window).scrollTop($realizationFromTop + $menuHeight);
+    }
+})
+
+$('.three').on('click', function () {
+    if ($(window).innerWidth() > 1024) {
+        $(window).scrollTop($locationsFromTop);
+    } else {
+        $(window).scrollTop($locationsFromTop + $menuHeight);
+    }
+})
+
+$('.four').on('click', function () {
+    if ($(window).innerWidth() > 1024) {
+        $(window).scrollTop($opinionsFromTop);
+    } else {
+        $(window).scrollTop($opinionsFromTop + $menuHeight);
+    }
+})
+
+$('.five').on('click', function () {
+    if ($(window).innerWidth() > 1024) {
+        $(window).scrollTop($contactFromTop);
+    } else {
+        $(window).scrollTop($contactFromTop + $menuHeight);
+    }
+})
